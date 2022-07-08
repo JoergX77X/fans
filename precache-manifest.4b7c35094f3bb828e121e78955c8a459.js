cb5bcc34a47d325c8fe6ec13531be9fcc8f58470
Thalionmel <wowbager@web.deecho>
2022-07-08 13:03:16 +0200

    Small changes

---
+++ precache-manifest.4b7c35094f3bb828e121e78955c8a459.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "64d274f40edabcbe2ea7",
+    "url": "/static/css/main.b6ca5656.chunk.css"
+  },
+  {
+    "revision": "64d274f40edabcbe2ea7",
+    "url": "/static/js/main.64d274f4.chunk.js"
+  },
+  {
+    "revision": "2d3efeb71c7eebfe7cd0",
+    "url": "/static/js/1.2d3efeb7.chunk.js"
+  },
+  {
+    "revision": "f8934aca82048141352e",
+    "url": "/static/css/2.19a65a33.chunk.css"
+  },
+  {
+    "revision": "f8934aca82048141352e",
+    "url": "/static/js/2.f8934aca.chunk.js"
+  },
+  {
+    "revision": "e279bb2fcb32ee98b7c8",
+    "url": "/static/js/runtime~main.e279bb2f.js"
+  },
+  {
+    "revision": "ec459ee0164b630aa5c7fb331cdf59e2",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fcf0fcc5ed41211de175ca1669b07cfd.js"
+  "/precache-manifest.4b7c35094f3bb828e121e78955c8a459.js"
 );
 
 workbox.clientsClaim();
